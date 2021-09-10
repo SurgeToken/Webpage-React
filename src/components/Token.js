385bf23abc0c1d9c89ee8b7877357b15eaea15da
johnnysheps <30838677+johnnysheps@users.noreply.github.com>
2021-09-10 12:41:46 -0400

    manifest update

--- src/components/Token.js
+++ src/components/Token.js
@@ -1,9 +1,9 @@
 function Token(props){
     return (
         <div>
-            <img className="sWordmark" src="{props.img}}" alt="surgeusd" />
+            <img className="sWordmark" src={props.img} alt="surgetoken" />
             <div className="contractAddress text-end">
-                <p className="cAddress" id="{props.tokenName}">{props.tokenAddress}  <i onClick={() => navigator.clipboard.writeText('{token.Address}')} className="far fa-copy"></i></p>
+                <p className="cAddress" id={props.tokenName}>{props.tokenAddress}  <i onClick={() => navigator.clipboard.writeText(props.tokenAddress)} className="far fa-copy"></i></p>
             </div>
         </div>
     )

f1fe823784f50f52f7fcd16de03345b96b0deb9e
johnnysheps <30838677+johnnysheps@users.noreply.github.com>
2021-09-11 11:50:58 -0400

    NavBar logos uploaded

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -14,7 +14,24 @@ function NavBar(){
                         <a href="https://app.xsurge.net">Trade</a>
                     </div>
                     <div className="navSocial">
-                        
+                        <a href="https://discord.gg/XSURGE">
+                            <img className="social1" src="assets/img/discord.png" alt="Discord Logo" />
+                        </a>
+                        <a href="https://instagram.com/XSURGEDEFI">
+                        <img className="social1" src="assets/img/instagram.png" alt="Instagram Logo" />
+                        </a>
+                        <a href="https://twitter.com/XSURGEDEFI">
+                        <img className="social1" src="assets/img/twitter.png" alt="Twitter Logo" />
+                        </a>
+                        <a href="https://www.reddit.com/r/XSURGE">
+                        <img className="social1" src="assets/img/reddit.png" alt="Reddit Logo" />
+                        </a>
+                        <a href="https://t.me/XSURGEDEFI">
+                        <img className="social1" src="assets/img/telegram.png" alt="Telegram Logo" />
+                        </a>
+                        <a href="https://www.facebook.com/groups/XSURGEDEFI">
+                        <img className="social1" src="assets/img/facebook.png" alt="Facebook Logo" />
+                        </a>
                     </div>
                 </div>
             </header>

0336a15400e42c1d735ca0d24e3abe4ec3222034
johnnysheps <30838677+johnnysheps@users.noreply.github.com>
2021-09-11 13:04:35 -0400

    Testing Updates Page

--- src/pages/updates.jsx
+++ src/pages/updates.jsx
@@ -1,10 +1,38 @@
 import React from "react";
+import Card from "../components/Card"
+import WidgetData from "../components/WidgetData";
+import { collection, onSnapshot } from "@firebase/firestore";
+import { useEffect, useState } from "react";
+import db from "../firebase";
 
 const Updates = () => {
+    const [blogs, setBlogs] = useState([]);
+
+  
+  useEffect(
+    () => 
+      onSnapshot(collection(db, "blogs"),(snapshot) => 
+        setBlogs(snapshot.docs.map((doc) => ({...doc.data(), id: doc.id})))
+      ),
+    []
+   );
   return (
-    <div>
-      Test Updates Page
-    </div>
+    <Card>
+            <WidgetData>
+              <div>
+                <h3>Latest Updates</h3>
+              </div>
+              {blogs.map((blog) => (
+                <div key={blog.id}>
+                  <h4>{blog.title}</h4>
+                  <h5>{blog.text}</h5>
+                  <hr/>
+                </div>
+                
+              ))}
+                
+            </WidgetData>
+          </Card>
   );
 };
 

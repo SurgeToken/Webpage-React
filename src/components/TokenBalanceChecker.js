8d343564546a75b9fd280c500d9342e80e240822
Birthday Smoothie <birthdaysmoothie22@icloud.com>
2021-11-29 01:12:06 +0000

    completed balance cards

--- src/components/TokenBalanceChecker.js
+++ src/components/TokenBalanceChecker.js
@@ -25,19 +25,22 @@ class TokenBalanceChecker extends Component {
 			check_balance_button_text_class: "",
 			selectedTokenByUser: false,
 			selectedToken: "",
-			token_balance_response: [],
+			token_balance_response: false,
 			check_balance_button_text: "",
 			capture_token_balance_container_input_id: "",
 			capture_token_balance_container_input_type: "",
-			capture_token_balance_input_placeholder: ""
+			capture_token_balance_input_placeholder: "",
+			capture_token_balance_input_value: "",
+			capture_token_balance_input_default_value: ""
 		};
 	}
 
 	tokenChange = (e) => {
 		this.setState({
-			token_balance_response: [],
+			token_balance_response: false,
 			error_message_class: "",
-			token_balance_container_class: "show"
+			token_balance_container_class: "show",
+			capture_token_balance_input_value: ""
 		});
 
 		let tokenSymbol = e.target.value;
@@ -119,6 +122,7 @@ class TokenBalanceChecker extends Component {
 		for (const token in tokens_to_check) {
 			wallet_response[tokens_to_check[token]["name"]] = {
 				'symbol': tokens_to_check[token]['symbol'],
+				'underlying_asset': tokens_to_check[token]['uasset'],
 				'decimals': tokens_to_check[token]['decimals'],
 				'decimal_display': tokens_to_check[token]['decimal_display']
 			};
@@ -192,12 +196,15 @@ class TokenBalanceChecker extends Component {
 				for (const k in wallet_response) {
 					if (wallet_response[k]['balance'] > 0) {
 						let balance = (wallet_response[k]['balance'] / 10**wallet_response[k]['decimals']);
-						let current_value = (balance * parseFloat(wallet_response[k]['token_price']) * parseFloat(wallet_response[k]['usd_price'])).toFixed(2);
+						let current_ua_amount = (balance * parseFloat(wallet_response[k]['token_price']));
+						let current_value = (balance * parseFloat(wallet_response[k]['token_price']) * parseFloat(wallet_response[k]['usd_price']));
 						output.push({
-							'name' : k, 
+							'name' : k,
+							'underlying_asset' : wallet_response[k]['underlying_asset'],
 							'symbol' : wallet_response[k]['symbol'],
-							'balance' : balance.toLocaleString(),
-							'current_value' : current_value.toLocaleString(),
+							'balance' : balance.toLocaleString(undefined, {maximumFractionDigits: 5}),
+							'underlying_asset_amount' : current_ua_amount.toLocaleString(undefined, {maximumFractionDigits: 5}), 
+							'current_value' : current_value.toLocaleString(undefined, {style: "currency", currency: "USD"}),
 							'token_price': wallet_response[k]['token_price']
 						});
 					}
@@ -234,9 +241,11 @@ class TokenBalanceChecker extends Component {
 				<div class={this.state.token_balance_container_class} id="token_balance_container">
 					{!this.renderCheckTokenBalancesView() ? "" :
 						<div id="capture_token_balance_container">
-							<input 
+							<input
 								class="capture_token_balance_input" 
-								id={this.state.capture_token_balance_container_input_id} 
+								id={this.state.capture_token_balance_container_input_id}
+								value={this.state.capture_token_balance_input_value}
+								default_value={this.state.capture_token_balance_input_default_value}
 								type={this.state.capture_token_balance_container_input_type}
 								placeholder={this.state.capture_token_balance_input_placeholder}
 							/>
@@ -252,24 +261,43 @@ class TokenBalanceChecker extends Component {
 						</div>
 					}
 
-					{this.state.token_balance_response == [] ? "" :
+					{!this.state.token_balance_response ? "" :
 						<div id="token_balance_display_container">
 							{this.state.token_balance_response.map((token) => {
 								return (
-
-									<div class="token_balance_container">
-										<div class="token_balance_token">
-											{token.name}
-										</div>
-										<div class="token_balance_header">
-											Current Balance
-										</div>
-										<div class="token_balance_amount">
-											{token.balance}
-										</div>
+									<div class="token_balance_wrapper">
+										<fieldset class="token_balance_wrapper_fieldset">
+											<legend align="center">{token.name}</legend>
+											<div class="token_balance_amount_wrapper">
+												<div class="token_balance_header">
+													Current Balance
+												</div>
+												<div class="token_balance_amount">
+													{token.balance}
+												</div>
+											</div>
+											<div class="token_balance_value_ua_wrapper">
+												<div class="token_value_ua_header">
+													Current Amount ({token.underlying_asset})
+												</div>
+												<div class="token_amount_ua">
+													{token.underlying_asset_amount}
+												</div>
+											</div>
+											<div class="token_balance_value_usd_wrapper">
+												<div class="token_value_usd_header">
+													Current Value (USD)
+												</div>
+												<div class="token_value_usd_amount">
+													{token.current_value}
+												</div>
+											</div>
+											<div class="clear"></div>
+										</fieldset>
 									</div>
 								);
 							})}
+							<p id="token_balance_disclaimer">*Data provided by BSC and CoinGecko API's</p>
 						</div>
 					}
 				</div>

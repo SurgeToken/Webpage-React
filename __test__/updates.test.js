b0be1930427825b9659c2dd5c27209513155716e
Weng Fei Fung <weffung@ucdavis.edu>
2021-12-25 17:33:05 -0800

    Test: Read updates mock folder by created times

--- __test__/updates.test.js
+++ __test__/updates.test.js
@@ -3,6 +3,39 @@ const glob = require('glob');
 const path = require('path')
 const { readdirSync } = require('fs')
 
+let FILE_MODES = {
+  CTIME: 1,
+  VERSION: 2
+} 
+
+function getMDFileUpdates(path, mode) {
+
+  let alphaSorted = fs.readdirSync(path).filter(function (filename) {
+    let isFile = file => fs.statSync(path+'/'+file).isFile(),
+      isHidden = file => (/(^|\/)\.[^\/\.]/g).test(file),
+      isMDFile = file => (/\.md$/g).test(file);
+    return !isHidden(filename) && isFile(filename) && isMDFile(filename);
+  });
+  
+  
+  if(mode===FILE_MODES.CTIME)
+    return alphaSorted.sort(function(filename, filename2) {
+      fileInfo = fs.statSync(path+'/'+filename);
+      fileInfo2 = fs.statSync(path+'/'+filename2)
+      // console.log({fileInfo,fileInfo2})
+      if(fileInfo.ctimeMs < fileInfo2.ctimeMs) {
+        return -1
+      } else if(fileInfo.ctimeMs > fileInfo2.ctimeMs) {
+        return 1
+      } else {
+        return 0;
+      }
+    });
+  else {
+    return alphaSorted;
+  }
+}
+
 describe('Test preliminary setup', () => {
 
     test('Test', () => {
@@ -11,16 +44,7 @@ describe('Test preliminary setup', () => {
 
 });
 
-describe('Read updates mock directory', () => {
-
-    function getMDFileUpdates(path) {
-        return fs.readdirSync(path).filter(function (ptr) {
-          let isFile = file => fs.statSync(path+'/'+file).isFile(),
-            isHidden = file => (/(^|\/)\.[^\/\.]/g).test(file),
-            isMDFile = file => (/\.md$/g).test(file);
-          return !isHidden(ptr) && isFile(ptr) && isMDFile(ptr);
-        });
-      }
+describe('Read updates mock folder', () => {
 
     let updatesPath = path.resolve(__dirname, "updates"); 
     // console.log({updatesPath})
@@ -37,6 +61,26 @@ describe('Read updates mock directory', () => {
         expect(updates[2]).toBe("c.md");
         expect(updates[3]).toBe("d-but-should-be-third.md");
     });
+
+});
+
+describe('Read updates mock folder by created times', () => {
+
+  let updatesPath = path.resolve(__dirname, "updates"); 
+  // console.log({updatesPath})
+  let updates = getMDFileUpdates(updatesPath, FILE_MODES.CTIME);
+  // console.log(updates);
+
+  test('MD files counted', () => {
+      expect(updates.length).toBe(4);
+  });
+
+  test('MD files matched', () => {
+      expect(updates[0]).toBe("a.md");
+      expect(updates[1]).toBe("b.md");
+      expect(updates[2]).toBe("d-but-should-be-third.md");
+      expect(updates[3]).toBe("c.md");
+  });
 
 });
 

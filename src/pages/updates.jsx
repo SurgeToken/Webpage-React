0e4da6b2688a0b204df7e969f443568d51d95c21
Weng Fei Fung <weffung@ucdavis.edu>
2021-12-25 20:36:09 -0800

    More stylized updates page

--- src/pages/updates.jsx
+++ src/pages/updates.jsx
@@ -3,27 +3,31 @@ import React from "react";
 //import WidgetData from "../components/WidgetData";
 import { Link } from "react-router-dom";
 import UpdateCard from "../components/UpdateCard";
+import "./updates.css"
 
 const Updates = () => {
     
   return (
     <div>
         <div className="fullscreen-bg">
-            <video loop muted autoPlay className="fullscreen-bg__video ">
+            <video loop muted autoPlay className="fullscreen-bg__video">
                 <source src="assets/vid/video4.mp4" type="video/mp4"/>
             </video>
+            <div className="hero-text">
+              <div className="piece">Updates</div>
+            </div>
         </div>
         
-        <div className="container">
-        
-        
+        <div className="container-wrapper">
+          <div className="container">
+            <UpdateCard></UpdateCard>
+            
 
-        <div className="dApp pad spacer5">
-            <Link to="/">Back</Link>
-        </div>
+            <button className="btn-back">
+                <Link to="/">Back</Link>
+            </button>
 
-        <UpdateCard>
-          </UpdateCard>
+          </div>
         </div>
     </div>
   );
